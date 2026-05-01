779d0a249010a5ae7e1c14d58f9a2ad4051d7f30
Mishalzahra28 <mishalzahra281@gmail.com>
2026-05-01 22:40:43 +0500

    refactor: update environment configuration to replace SUPABASE_URL with NEXT_PUBLIC_SUPABASE_URL

--- src/env.ts
+++ src/env.ts
@@ -3,18 +3,18 @@ import { z } from 'zod';
 
 export const env = createEnv({
   server: {
-    SUPABASE_URL: z.string().url(),
     SUPABASE_ANON_KEY: z.string().min(1),
     SUPABASE_SERVICE_ROLE_KEY: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string(),
     NEXT_PUBLIC_APP_NAME: z.string(),
+    NEXT_PUBLIC_SUPABASE_URL: z.string().url(),
   },
   runtimeEnv: {
     NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL,
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
-    SUPABASE_URL: process.env.SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
     SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY,
     SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
   },
